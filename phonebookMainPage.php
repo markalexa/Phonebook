0ac33a38bd991772e48409fe396e67147cff0970
markalexa <mark.alexa.uk@gmail.com>
2017-08-05 17:31:07 +0100

    editing main page

--- phonebookMainPage.php
+++ phonebookMainPage.php
@@ -86,49 +86,56 @@ mysqli_close($link);
     integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
 	 integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<!-- <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">-->
+	<<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
 	 <title>Online Phonebook</title>
 	 <style type="text/css">
-	 * {font-size: 18px !important;}  /*  <--- TAKE THIS OFF !  */
-	 	body {font-family: 'Dancing Script', cursive;background-image: url("phone.jpg");background-position: right;background-repeat: no-repeat;
+		#loginBtn {padding: 0 10px;border-radius:7px;font-size:25px;font-family: 'Dancing Script', Arial;bottom:10px;}
+	 	body {font-family: 'Dancing Script', cursive;background-image: url("phone.jpg");background-position: center center;background-repeat: no-repeat;
 	 	background-size: cover;}
-	 	#border {margin-left:10px;border 1px solid white;border-radius: 8px;}
+	 	#text {margin-left:10px;}
 	 	h1 , h2 , p {padding: 10px;}
 	 	h1 {font-size: 60px;}
 	 	h2 {font-size: 50px;}
 	 	#fromLeft {margin-left: 120px;}
 	 	h2 , h3 {margin-left: 100px;}
 	 	h3 {font-size: 40px;}
-	 	/*.jumbotron {background-color: transparent;text-align: left;}*/
-	 	form {width: 300px;}
-	 	label {font-size: 40px;}
-	 	input {margin-bottom: 10px;}
-	 	#submit {margin-top: 10px;font-size: 30px;}
+	 	.container {margin-left:10px !important;margin-bottom:20px;width:250px !important;}
+	 	span {color: white;}
+	 	label {font-size: 40px;color: white;}
+	 	input {margin-bottom: 10px;font-family: Arial;font-size: 10px;}
+	 	#submit {margin-top: 10px;border-radius:8px;margin-left:30px;font-size: 30px;font-family: 'Dancing Script', Arial;background: #cea337;border-color:transparent;}
+		#submit:hover {background:#d6a731;}	 	
 	 	#login {display:none;margin-left: 30px;margin-top: 60px;}
+	 	#login label {color:#4c3806;}
 	 	#signup {margin-left: 30px;margin-top: 60px;}
+	 	#switch {font-size:25px;text-decoration: none;font-weight: bold;color:#4c3806;}
 	 </style>
     
   </head>
   <body>
-  	<!--<div id="border">
-				<h1>Save your friend's or <span style="color:white;">family</span><br> member's phone number.</h1>
+  <a id="switch">Log In</a>
+  	<div id="text">
+				<h1>Save your phone numb<span>ers</span><br>into the Online Pho<span>nebook.</span></h1>
+				<h1>Securely. For free.</h1>
 				<h2>First timer ?</h2><?php echo $success; ?><h1 id="fromLeft">Sign Up !</h1>
 				<h3>or log in to add new numbers</h3>
-				</div>-->
+	</div>
     
-    <div class="container" style="width:300px;">
+    <div class="container">
     
     	<form method="post" action="phonebookMainPage.php" id="login" style="margin-bottom:40px;">
 			<div class="form-group">
-			<label>Username: </label><input class="form-control" type="text" name="usernameToCheck" required>
+			<label>Username: </label><input class="form-control" autofocus="autofocus" type="text" name="usernameToCheck" required>
 			<label>Password: </label><input class="form-control" type="password" name="passwordToCheck" required>
-			<input type="submit" name="submit" value="Log In" class="form-control">
+			<input id="loginBtn" type="submit" name="submit" value="Log In">
+			
+			</div>
 		</form>
    
 		<form method="post" action="phonebookMainPage.php" id="signup">
 			<div class="form-group">
 				
-				<label>Choose Your Username</label>
+				<label><span style="color:black;">Choose Your</span> Username</label>
 				<input type="text" class="form-control" name="username" alt="username" autofocus="autofocus" required>
 				<label>Your Email</label>
 				<input type="email" class="form-control" name="email" alt="email" required>
@@ -144,5 +151,13 @@ mysqli_close($link);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
     integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    	$("#switch").on('click',function () {
+    		$("#switch").html(($("#switch").html() == 'Log In') ? 'Back to Sign Up' : 'Log In');
+    		$("#signup").toggle();
+    		$("#login").toggle();
+    		$("#text").toggle();
+    	});
+    </script>
   </body>
 </html>
